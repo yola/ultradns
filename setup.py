e094679810d9c2bc2ea7913f5422869e75b9f499
Eugene Koval <eugene@yola.com>
2014-09-02 13:58:54 +0000

    Fix version. Bump minor version, not release version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='youltradns',
-    version='0.0.2',
+    version='0.1.0',
     description='Yola UltraDNS Client',
     author='Yola',
     author_email='engineers@yola.com',
