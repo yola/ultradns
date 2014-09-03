ff70c3424d0f685891ee3bdaa5405bdd45a4bb3c
Eugene Koval <eugene@yola.com>
2014-09-03 13:00:22 +0000

    Remove base class, which actually was just an interface description. Because Seryoha said so.

--- youltradns/client.py
+++ youltradns/client.py
@@ -2,7 +2,6 @@ import json
 
 import requests
 
-from client_base import BaseUltraDNSClient
 from exceptions import (ZoneNotFoundError, ZoneAlreadyExistsError,
                         PermissionDeniedError, DNSError, TransactionError,
                         TransactionAlreadyInProgressError,
@@ -94,7 +93,7 @@ class UltraDNSAuthentication(ErrorHandlingMixin):
             raise AuthenticationError(json_body)
 
 
-class UltraDNSClient(BaseUltraDNSClient, ErrorHandlingMixin):
+class UltraDNSClient(ErrorHandlingMixin):
     _auth_class = UltraDNSAuthentication
 
     def __init__(self, username, password, url='https://restapi.ultradns.com'):
@@ -110,6 +109,11 @@ class UltraDNSClient(BaseUltraDNSClient, ErrorHandlingMixin):
             self.get_account_details()[u'accounts'][0][u'accountName']
 
     def create_primary_zone(self, zone_name):
+        """Creates a new primary zone.
+
+        Arguments:
+            zone_name -- The name of the zone.  It must be unique.
+        """
         zone_properties = {'name': zone_name,
                            'accountName': self._account_name,
                            'type': 'PRIMARY'}
@@ -119,6 +123,29 @@ class UltraDNSClient(BaseUltraDNSClient, ErrorHandlingMixin):
         self._post('/v1/zones', zone_data)
 
     def get_zones_of_account(self, account_name, q=None, **kwargs):
+        """Returns a list of zones for the specified account.
+
+        Arguments:
+            account_name -- The name of the account.
+
+        Keyword Arguments:
+            q -- The search parameters, in a dict.  Valid keys are:
+                 name - substring match of the zone name
+                 zone_type - one of:
+                    PRIMARY
+                    SECONDARY
+                    ALIAS
+            sort -- The sort column used to order the list. Valid values for
+                    the sort field are:
+                    NAME
+                    ACCOUNT_NAME
+                    RECORD_COUNT
+                    ZONE_TYPE
+            reverse -- Whether the list is ascending(False) or descending(True)
+            offset -- The position in the list for the first returned
+                      element(0 based)
+            limit -- The maximum number of rows to be returned.
+        """
         uri = '/v1/accounts/' + account_name + '/zones'
         params = self._build_params(q, kwargs)
         return self._get(uri, params)
@@ -127,9 +154,34 @@ class UltraDNSClient(BaseUltraDNSClient, ErrorHandlingMixin):
         return self._get('/v1/zones/' + zone_name)
 
     def delete_zone(self, zone_name):
+        """Deletes the specified zone.
+
+        Arguments:
+            zone_name -- The name of the zone being deleted.
+        """
         self._delete('/v1/zones/' + zone_name)
 
     def get_records(self, zone_name, rtype=None, q=None, **kwargs):
+        """Returns the list of records in the specified zone.
+
+        Arguments:
+            zone_name -- The name of the zone.
+
+        Keyword Arguments:
+            q -- The search parameters, in a dict.  Valid keys are:
+                 ttl - must match the TTL for the rrset
+                 owner - substring match of the owner name
+                 value - substring match of the first BIND field value
+            sort -- The sort column used to order the list. Valid values for
+                    the sort field are:
+                    OWNER
+                    TTL
+                    TYPE
+            reverse -- Whether the list is ascending(False) or descending(True)
+            offset -- The position in the list for the first returned
+                      element(0 based)
+            limit -- The maximum number of rows to be returned.
+        """
         uri = '/v1/zones/' + zone_name + '/rrsets'
         if rtype:
             uri += '/' + rtype
@@ -141,6 +193,28 @@ class UltraDNSClient(BaseUltraDNSClient, ErrorHandlingMixin):
             return []
 
     def create_record(self, zone_name, rtype, owner_name, rdata, ttl=None):
+        """Creates a new record in the specified zone.
+
+        Arguments:
+            zone_name -- The zone that will contain the new record.
+                         The trailing dot is optional.
+            rtype -- The type of the record.  This can be numeric (1) or
+                     if a well-known name is defined for the type (A), you can
+                     use it instead.
+            owner_name -- The owner name for the record.
+                          If no trailing dot is supplied, the owner_name is
+                          assumed to be relative (foo).
+                          If a trailing dot is supplied, the owner name is
+                          assumed to be absolute (foo.zonename.com.)
+            rdata -- The BIND data for the record as a string.
+                     If there is a single resource record in the record, you
+                     can pass in the single string.
+                     If there are multiple resource records  in this record,
+                     pass in a list of strings.
+
+        Keyword Arguments:
+            ttl -- The TTL value for the record.
+        """
         if not isinstance(rdata, list):
             rdata = [rdata]
 
@@ -152,6 +226,28 @@ class UltraDNSClient(BaseUltraDNSClient, ErrorHandlingMixin):
                    owner_name, record)
 
     def edit_record(self, zone_name, rtype, owner_name, rdata, ttl=None):
+        """Updates an existing record in the specified zone.
+
+        Arguments:
+            zone_name -- The zone that contains the record.  The trailing dot
+                         is optional.
+            rtype -- The type of the record.  This can be numeric (1) or
+                     if a well-known name is defined for the type (A), you can
+                     use it instead.
+            owner_name -- The owner name for the record.
+                          If no trailing dot is supplied, the owner_name is
+                          assumed to be relative (foo).
+                          If a trailing dot is supplied, the owner name is
+                          assumed to be absolute (foo.zonename.com.)
+            rdata -- The updated BIND data for the record as a string.
+                     If there is a single resource record in the record, you
+                     can pass in the single string.
+                     If there are multiple resource records  in this record,
+                     pass in a list of strings.
+
+        Keyword Arguments:
+            ttl -- The updated TTL value for the record.
+        """
         if type(rdata) is not list:
             rdata = [rdata]
 
@@ -163,6 +259,20 @@ class UltraDNSClient(BaseUltraDNSClient, ErrorHandlingMixin):
         return self._put(uri, record)
 
     def delete_record(self, zone_name, rtype, owner_name):
+        """Deletes an record.
+
+        Arguments:
+            zone_name -- The zone containing the record to be deleted.
+                         The trailing dot is optional.
+            rtype -- The type of the record.  This can be numeric (1) or
+                     if a well-known name is defined for the type (A), you can
+                     use it instead.
+            owner_name -- The owner name for the record.
+                          If no trailing dot is supplied, the owner_name is
+                          assumed to be relative (foo).
+                          If a trailing dot is supplied, the owner name is
+                          assumed to be absolute (foo.zonename.com.)
+        """
         return self._delete('/v1/zones/' + zone_name + '/rrsets/' + rtype +
                             '/' + owner_name)
 
@@ -180,12 +290,14 @@ class UltraDNSClient(BaseUltraDNSClient, ErrorHandlingMixin):
         return self._get('/v1/status')
 
     def start_transaction(self):
+        """ Start transaction. """
         if self._transaction:
             raise TransactionAlreadyInProgressError()
         self._transaction_queries = []
         self._transaction = True
 
     def commit_transaction(self):
+        """ Commit transaction. """
         if not self._transaction:
             raise NoActiveTransactionError()
         elif not self._transaction_queries:
@@ -194,17 +306,18 @@ class UltraDNSClient(BaseUltraDNSClient, ErrorHandlingMixin):
         result = self._run_transaction_queries()
         return result
 
+    def rollback_transaction(self):
+        """ Rollback transaction. """
+        if not self._transaction:
+            raise NoActiveTransactionError()
+        self._end_transaction()
+
     def _end_transaction(self):
         if not self._transaction:
             raise NoActiveTransactionError()
 
         self._transaction_queries = []
         self._transaction = False
-
-    def rollback_transaction(self):
-        if not self._transaction:
-            raise NoActiveTransactionError()
-        self._end_transaction()
 
     def _authenticate(self):
         self._auth.authenticate()
